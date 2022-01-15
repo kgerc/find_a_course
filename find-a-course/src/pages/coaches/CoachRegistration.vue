082f2aeb33924c348663eb20ca71c90c550f3442
Krzysztof Gerc <krzysztof.gerc@euvic.pl>
2022-01-15 19:03:03 +0100

    Added filter to the list of teachers and teacher registration form

--- find-a-course/src/pages/coaches/CoachRegistration.vue
+++ find-a-course/src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,15 @@
 <template>
-    REGISTER
+    <BaseCard>
+        <h2>Register as a teacher now!</h2>
+        <CoachForm></CoachForm>
+    </BaseCard>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+export default {
+    components: {
+        CoachForm
+    }
+}
+</script>
