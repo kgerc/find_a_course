9ade74e8a83198b95ef32887bfeac47524818081
Krzysztof Gerc <krzysztof.gerc@euvic.pl>
2022-01-15 16:13:29 +0100

    added details page for each teacher

--- find-a-course/src/pages/coaches/CoachDetail.vue
+++ find-a-course/src/pages/coaches/CoachDetail.vue
@@ -1,5 +1,54 @@
 <template>
-    DETAILS FOR TEACHER
-    <router-view></router-view>
-    <router-link to="/coaches/c1/contact">Contact</router-link>
+    <section>
+        <BaseCard>
+            <h2>{{ fullName }}</h2>
+            <h3>${{ rate }}/hour</h3>
+        </BaseCard>
+    </section>
+    <section>
+        <BaseCard>
+            <header>
+                <h2>Interested? Reach out now!</h2>
+                <BaseButton link :to="contactLink">Contact</BaseButton>
+            </header>
+            <router-view></router-view>
+        </BaseCard>
+    </section>
+    <section>
+        <BaseCard>
+            <BaseBadge v-for="area in areas" :key="area" :type="area" :title="area"></BaseBadge>
+            <p>{{ description }}</p>
+        </BaseCard>
+    </section>
 </template>
+
+<script>
+export default {
+    props: ['id'],
+    data() {
+        return {
+            selectedCoach: null
+        };
+    },
+    computed: {
+        fullName() {
+            return this.selectedCoach.firstName + ' ' + this.selectedCoach.lastName;
+        },
+        contactLink() {
+            return this.$route.path + '/' + this.id + '/contact';
+        },
+        areas() {
+            return this.selectedCoach.areas;
+        },
+        rate() {
+            return this.selectedCoach.hourlyRate;
+        },
+        description() {
+            return this.selectedCoach.description;
+        }
+    },
+    created() {
+        this.selectedCoach = this.$store.getters['coaches/coaches'].find(coach => coach.id === this.id);
+    },
+}
+</script>
