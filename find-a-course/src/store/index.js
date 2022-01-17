b86ca3ab568c525d2662705e547d3c787c953ca8
Krzysztof Gerc <krzysztof.gerc@euvic.pl>
2022-01-17 21:51:56 +0100

    Added contact teacher view

--- find-a-course/src/store/index.js
+++ find-a-course/src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore } from 'vuex';
 import coachesModule from './modules/coaches/index.js';
+import requestsModule from './modules/';
 const store = createStore({
     modules: {
-        coaches: coachesModule
+        coaches: coachesModule,
+        requests: requestsModule
     },
     state() {
         return {
