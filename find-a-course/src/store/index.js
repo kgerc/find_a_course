915f2176609fc2bdc04f62f3a11e321624221524
Krzysztof Gerc <krzysztof.gerc@euvic.pl>
2022-01-17 21:54:04 +0100

    changed the directory of requests folder

--- find-a-course/src/store/index.js
+++ find-a-course/src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore } from 'vuex';
 import coachesModule from './modules/coaches/index.js';
-import requestsModule from './modules/';
+import requestsModule from './modules/requests/index.js';
 const store = createStore({
     modules: {
         coaches: coachesModule,
